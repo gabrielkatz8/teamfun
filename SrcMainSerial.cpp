94927930b816383eff52e89d4c6f7bef201ab31b
gabrielkatz8 <gabriejk@usc.edu>
2020-04-19 14:04:02 -0700

    edit serial file

--- SrcMainSerial.cpp
+++ SrcMainSerial.cpp
@@ -1,101 +1,130 @@
 #include "SrcMain.h"
 #include "HashTable.hpp"
 #include "sha1.h"
-#include <tbb/parallel_for_each.h>
+#include <unordered_map>
 #include <iostream>
 #include <fstream>
 #include <ostream>
 #include <vector>
 
-void ProcessCommandArgs(int argc, const char* argv[])
+int main()
 {
-	// TODO: Implement
-    std::string type = argv[1];
-    int size = atoi(argv[2]);
-    if (type == "dictionary") {
-        HashTable<std::string> h(size);
-        std::string dictFile = argv[3];
-        std::string passFile = argv[4];
+        //std::string phrasesFile = argv[3];
+        std::string pFile = "passwords.txt";
         
-        std::ifstream ifile(dictFile);
-        std::string line;
-        while (static_cast<bool>(getline(ifile, line))) {
-            unsigned char hash[20];
-            sha1::Calc(line.c_str(), line.length(), hash);
-            
-            std::string hexStr;
-            hexStr.resize(40);
-            sha1::ToHexString(hash, &hexStr[0]);
-            
-            h.Insert(hexStr, line);
-        }
-        ifile.close();
-        
-        std::ifstream pfile(passFile);
-        std::ofstream ofile("solved.txt");
-        std::string word;
-        while (static_cast<bool>(getline(pfile, word))) {
-            ofile << word << "," << *h.Find(word) << "\n";
-        }
-        pfile.close();
-        ofile.close();
-    }
-    else if (type == "phrases") {
-        std::string phrasesFile = argv[3];
-        std::string pFile = argv[4];
-        
-        HashTable<int> h(size);
-        
-        std::ifstream phraseFile(phrasesFile);
-        std::string p;
-        std::vector<std::string> allPhrases;
-        while (static_cast<bool>(getline(phraseFile, p))) {
-            allPhrases.emplace_back(p);
-        }
-        
+        std::string ch = "0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+
+        std::unordered_map<std::string, std::string> h(20);
+
         std::ifstream ifile(pFile);
         std::string line;
-        int i = 0;
-        
-        while (static_cast<bool>(getline(ifile, line))) {
-            
-            h.Insert(line, i);
-            i++;
+        int numPasswords = 0;
+
+        while (getline(ifile, line)) {
+        	unsigned char hash[20];
+        	sha1::Calc(line.c_str(), line.length(), hash);
+        	std::string hexStr;
+        	hexStr.resize(40);
+        	sha1::ToHexString(hash, &hexStr[0]);
+        	h.insert({hexStr, line});
+        	//h[hexStr] = line;
+        	numPasswords++;
         }
-        ifile.close();
         
-        std::vector<std::pair<std::string, std::string>> solved(h.Size());
+        // std::ifstream phraseFile(phrasesFile);
+        // std::string p;
+        // std::vector<std::string> allPhrases;
+        // while (static_cast<bool>(getline(phraseFile, p))) {
+        //     allPhrases.emplace_back(p);
+        // }
         
-        std::string fourPhrases;
+        // std::ifstream ifile(pFile);
+        // std::string line;
+        // int i = 0;
+        
+        // while (static_cast<bool>(getline(ifile, line))) {
+            
+        //     h.Insert(line, i);
+        //     i++;
+        // }
+        // ifile.close();
+        
+        std::vector<std::pair<std::string, std::string>> solved;
+        std::vector<int> indices(1);
+        indices[0] = 0;
+        
+        // std::string fourPhrases;
 
-        tbb::parallel_for_each(allPhrases.begin(), allPhrases.end(), [&solved, &h, &allPhrases](std::string word1) {
-            tbb::parallel_for_each(allPhrases.begin(), allPhrases.end(), [&solved, &h, &allPhrases, &word1](std::string word2) {
-                tbb::parallel_for_each(allPhrases.begin(), allPhrases.end(), [&solved, &h, &allPhrases, &word1, &word2](std::string word3) {
-                    unsigned int allPhrasesSize = allPhrases.size();
-                    std::string test;
-                    std::string hexStr;
-                    int* val;
-                    unsigned char hash[20];
-                    unsigned int j;
-                    for (j = 0; j < allPhrasesSize; j++) {
-                        //test = word1 + word2 + allPhrases[k] + allPhrases[j];
-                        test = word1 + word2 + word3 + allPhrases[j];
+        // tbb::parallel_for_each(allPhrases.begin(), allPhrases.end(), [&solved, &h, &allPhrases](std::string word1) {
+        //     tbb::parallel_for_each(allPhrases.begin(), allPhrases.end(), [&solved, &h, &allPhrases, &word1](std::string word2) {
+        //         tbb::parallel_for_each(allPhrases.begin(), allPhrases.end(), [&solved, &h, &allPhrases, &word1, &word2](std::string word3) {
+        //             unsigned int allPhrasesSize = allPhrases.size();
+        //             std::string test;
+        //             std::string hexStr;
+        //             int* val;
+        //             unsigned char hash[20];
+        //             unsigned int j;
+        //             for (j = 0; j < allPhrasesSize; j++) {
+        //                 test = word1 + word2 + word3 + allPhrases[j];
 
-                        sha1::Calc(test.c_str(), test.length(), hash);
+        //                 sha1::Calc(test.c_str(), test.length(), hash);
 
                         
-                        hexStr.resize(40);
-                        sha1::ToHexString(hash, &hexStr[0]);
-                        val = h.Find(hexStr);
-                        if (val != nullptr) {
-                            solved[*val].first = hexStr;
-                            solved[*val].second = test;
-                        }
-                    }
-                //}
-                });
-            });
-        });
+        //                 hexStr.resize(40);
+        //                 sha1::ToHexString(hash, &hexStr[0]);
+        //                 val = h.Find(hexStr);
+        //                 if (val != nullptr) {
+        //                     solved[*val].first = hexStr;
+        //                     solved[*val].second = test;
+        //                 }
+        //             }
+        //         //}
+        //         });
+        //     });
+        // });
+
+        bool done = false;
+        while (!done) {
+        	if (numPasswords == 0) {
+        		done = true;
+        	}
+        	else {
+        		std::string attempt = "";
+        		for (int i = 0; i < indices.size(); i++) {
+        			attempt += ch[indices[i]];
+        		}
+        		unsigned char hash[20];
+	        	sha1::Calc(attempt.c_str(), attempt.length(), hash);
+	        	std::string hexStr;
+	        	hexStr.resize(40);
+	        	sha1::ToHexString(hash, &hexStr[0]);
+	        	if (h.find(hexStr) != h.end()) {
+	        		std::pair<std::string, std::string> newPair = std::make_pair(h[hexStr], hexStr);
+	        		solved.push_back(newPair);
+	        		numPasswords--;
+	        	}
+	        	indices[indices.size() - 1]++;
+	        	if (indices[indices.size() - 1] == ch.size()) {
+	        		indices[indices.size() - 1] = 0;
+	        		bool finished = false;
+	        		int cnt = 2;
+	        		while (finished != false) {
+	        			if (indices.size() - cnt < 0) {
+	        				done = true;
+	        				finished = true;
+	        			}
+	        			else if (indices[indices.size() - cnt] == ch.size() - 1) {
+	        				indices[indices.size() - cnt] = 0;
+	        				cnt--;
+	        			}
+	        			else {
+	        				indices[indices.size() - cnt]++;
+	        				finished = true;
+	        			}
+	        		}
+	        	}
+        	}
+        }
         
         std::ofstream ofile("solved.txt");
         
@@ -106,6 +135,6 @@ void ProcessCommandArgs(int argc, const char* argv[])
         
         ofile.close();
         
-    }
+    //}
     
 }
