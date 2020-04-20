06cb1bb9d9ec45ccedce0f7ccaa9a8c5e6dcc110
Drew <charters@usc.edu>
2020-04-19 23:02:37 -0700

    updated main file for sha test

---
+++ main_sha.cpp
@@ -0,0 +1,33 @@
+#include "sha1.h"
+#include "sha1_parallel.h"
+#include <vector>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+int main(void) {
+    //get list of strings to hash
+    vector<string> strings_to_hash;
+
+    //initialize timing variables to compare the times of the serial and parallel algorithms
+    struct timespec start, stop;
+    double time;
+
+    for (unsigned int i = 0; i < strings_to_hash.size(); i++) {
+        //test serial time and print results
+        if(clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+        //EXECUTION
+        if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}		
+		time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
+        printf("Execution time = %f sec\n", time);	
+
+        //test parallel time and print results
+        if(clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+        //EXECUTION
+        if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}		
+		time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
+        printf("Execution time = %f sec\n", time);	
+
+    }
+}
